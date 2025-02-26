15af52e7bc5dad2d849a55e496428f736b06b543
curtis-taylor <curtis.t@gmail.com>
2025-02-25 19:25:13 -0500

    Added e2e test configurations and folders

---
+++ tests/profiles.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+const url_val = "http://localhost:3000/pages/profile/";
+
+test('has title', async ({ page }) => {
+    await page.goto(url_val);
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Profile/);
+
+    await page.getByRole('link', { name: 'Team John Doe Avatar John Doe' }).first().click();
+  });
+
