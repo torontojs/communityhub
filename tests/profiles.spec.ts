2911efed18e56e9efb339b1b4ed9cdc496abffeb
curtis-taylor <curtis.t@gmail.com>
2025-02-25 19:30:54 -0500

    Format test files

--- tests/profiles.spec.ts
+++ tests/profiles.spec.ts
@@ -1,13 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-const url_val = "http://localhost:3000/pages/profile/";
+const url_val = 'http://localhost:3000/pages/profile/';
 
 test('has title', async ({ page }) => {
-    await page.goto(url_val);
-  
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Profile/);
+	await page.goto(url_val);
 
-    await page.getByRole('link', { name: 'Team John Doe Avatar John Doe' }).first().click();
-  });
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Profile/);
 
+	await page.getByRole('link', { name: 'Team John Doe Avatar John Doe' }).first().click();
+});
--- tests/team.spec.ts
+++ tests/team.spec.ts
@@ -1,12 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-const url_val = "http://localhost:3000/pages/team/";
+const url_val = 'http://localhost:3000/pages/team/';
 
 test('Select ', async ({ page }) => {
-    await page.goto(url_val);
-  
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Team/);
+	await page.goto(url_val);
 
-    await page.getByRole('link', { name: 'Team John Doe Avatar John Doe' }).first().click();
-  });
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Team/);
+
+	await page.getByRole('link', { name: 'Team John Doe Avatar John Doe' }).first().click();
+});
