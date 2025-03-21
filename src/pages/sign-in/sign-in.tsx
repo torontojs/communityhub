df65cccc5efdfda82497b3cdcd84c997a4a56722
Lyla <mjuudev@gmail.com>
2025-03-21 17:22:13 -0400

    feat: add a basic structure of sign-in page
    Co-authored-by: Manvinderjit Rupal <manvinderjit@users.noreply.github.com>

---
+++ src/pages/sign-in/sign-in.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import 'open-props';
+import FooterNav from '../../components/Footer/Footer';
+import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
+import Logo from '../../components/Logo/Logo';
+import '../../index.css';
+import './style.css';
+
+const root = document.getElementById('root') as HTMLDivElement;
+
+createRoot(root).render(
+	(
+		<StrictMode>
+			<div className='App'>
+				<header>
+					<Logo />
+					<HeaderTitle>Welcome to TorontoJS Community Hub</HeaderTitle>
+				</header>
+
+				<FooterNav />
+			</div>
+		</StrictMode>
+	)
+);
