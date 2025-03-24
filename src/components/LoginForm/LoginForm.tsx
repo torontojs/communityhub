5977786e53368c123e5bf421ad6cc10237eb3bd1
kbventures <kmb_x@hotmail.com>
2025-03-24 11:15:30 -0400

    Login Form Structure

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,28 +1,29 @@
 import './LoginForm.css';
 
-
 const LoginForm = () => (
-    <form className="login-form">
-    <p>Enter your e-mail and password to log-in.</p>
-    <div>
-        <label htmlFor='email'>Email</label>
-        <input type='email' id='email' name='email' />
-    </div>
-    <div>
-        <label htmlFor='password'>Password</label>
-        <input type='password' id="password" name="password" />
-    </div>
-    <div>
-        <label htmlFor="remember-me">Remember my password</label>
-        <input type="checkbox" id="remember-me" name="remember-me" />
-    </div>
-    <button type='submit'>Log-in</button>
-    <link ref='/forgot-password'>I don't remember my password</link>
-    <div id="line-seperator"></div>
-    <div>
-        <p>If you're a member of TorontoJS and don't have your account, <link>click here to sign up</link></p>
-    </div>
-    </form>
+	<form className='login-form'>
+		<p>Enter your e-mail and password to log-in.</p>
+		<div>
+			<label htmlFor='email'>Email</label>
+			<input type='email' id='email' name='email' />
+		</div>
+		<div>
+			<label htmlFor='password'>Password</label>
+			<input type='password' id='password' name='password' />
+		</div>
+		<div>
+			<label htmlFor='remember-me'>Remember my password</label>
+			<input type='checkbox' id='remember-me' name='remember-me' />
+		</div>
+		<button type='submit'>Log-in</button>
+		<link ref='/forgot-password'>I don't remember my password</link>
+		<div id='line-seperator'></div>
+		<div>
+			<p>
+				If you're a member of TorontoJS and don't have your account, <link>click here to sign up</link>
+			</p>
+		</div>
+	</form>
 );
 
 export default LoginForm;
--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import 'open-props';
 import FooterNav from '../../components/Footer/Footer';
 import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
-import Logo from '../../components/Logo/Logo';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import Logo from '../../components/Logo/Logo';
 import '../../index.css';
 import './style.css';
 
@@ -14,12 +14,12 @@ createRoot(root).render(
 	(
 		<StrictMode>
 			<div className='App'>
-			<header>
+				<header>
 					<Logo />
 					<HeaderTitle>Welcome to TorontoJS Community Hub</HeaderTitle>
-			</header>
-			<LoginForm/>
-			<FooterNav/>
+				</header>
+				<LoginForm />
+				<FooterNav />
 			</div>
 		</StrictMode>
 	)
