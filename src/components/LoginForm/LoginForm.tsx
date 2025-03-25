221ce603f52ebf5630a71307bf815b376badeb70
kbventures <kmb_x@hotmail.com>
2025-03-25 17:02:35 -0400

    Draft: Sign-in page

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -6,19 +6,18 @@ const LoginForm = () => (
 		<p className='center'>Enter your e-mail and password to log-in.</p>
 		<div className='inputDim'>
 			<label className='block' htmlFor='email'>E-mail</label>
-			<input type='email' id='email' name='email' />
+			<input type='email' id='email' name='email' placeholder="Your account e-mail"/>
 		</div>
 		<div className='inputDim'>
-			<label className='block' htmlFor='password'>Password</label>
-			<input type='password' id='password' name='password' />
+		<label className='block' htmlFor='password'>Password</label>
+			<input type='password' id='password' name='password' placeholder="Your password" />
 		</div>
-		<div className='inputDim'>
-			<label className='block' htmlFor='remember-me'>Remember my password</label>
+		<div className='inputDimRadio flex'>
 			<input type='checkbox' id='remember-me' name='remember-me' />
+			<label htmlFor='remember-me'>Remember my password</label>
 		</div>
 		<Button type='submit' isLarge={true} style={{ color: 'white', background: '#ED342F' }}>Log In</Button>
-		<a href='/forgot-password'>I don't remember my password</a>
-		<div id='line-seperator'></div>
+		<a className="center block forgot-password" href='/forgot-password'>I don't remember my password</a>
 		<div>
 			<p>
 				If you're a member of TorontoJS and don't have your account, <a href='/path-to-other-page'>Click here to go to another page</a>
