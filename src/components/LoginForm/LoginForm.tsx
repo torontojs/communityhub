cd7ba1fa4aebdc3cd345563f42f45feb9e783fd2
kbventures <kmb_x@hotmail.com>
2025-03-25 11:47:09 -0400

    Feat Draft Update: Sign In Form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,27 +1,27 @@
 import './LoginForm.css';
+import Button from '../Button/Button'
 
 const LoginForm = () => (
 	<form className='login-form'>
-		<p>Enter your e-mail and password to log-in.</p>
-		<div>
-			<label htmlFor='email'>Email</label>
+		<p className="center">Enter your e-mail and password to log-in.</p>
+		<div className='inputDim'>
+			<label className='block' htmlFor='email'>E-mail</label>
 			<input type='email' id='email' name='email' />
 		</div>
-		<div>
-			<label htmlFor='password'>Password</label>
+		<div className='inputDim'>
+			<label className='block' htmlFor='password'>Password</label>
 			<input type='password' id='password' name='password' />
 		</div>
-		<div>
-			<label htmlFor='remember-me'>Remember my password</label>
+		<div className='inputDim'>
+			<label className='block' htmlFor='remember-me'>Remember my password</label>
 			<input type='checkbox' id='remember-me' name='remember-me' />
 		</div>
-		<button type='submit'>Log-in</button>
+		<Button type='submit' isLarge={true} style={{color: 'white', background: '#ED342F'}}>Log In</Button>
 		<a href='/forgot-password'>I don't remember my password</a>
 		<div id='line-seperator'></div>
 		<div>
 			<p>
 				If you're a member of TorontoJS and don't have your account, <a href="/path-to-other-page">Click here to go to another page</a>
-
 			</p>
 		</div>
 	</form>
