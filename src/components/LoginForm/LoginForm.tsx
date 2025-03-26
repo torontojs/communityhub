1e1600c214af67e65966906c2b7774594050aa4c
kbventures <kmb_x@hotmail.com>
2025-03-26 12:30:29 -0400

    Draft: Sign-in

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -14,14 +14,14 @@ const LoginForm = () => (
 		</div>
 		<div className='inputDimRadio flex'>
 			<input type='checkbox' id='remember-me' name='remember-me' />
-			<label htmlFor='remember-me' className="padding-left-2px">Remember my password</label>
+			<label htmlFor='remember-me' className='padding-left-2px'>Remember my password</label>
 		</div>
 		<Button type='submit' isLarge={true} style={{ color: 'white', background: '#ED342F' }}>Log In</Button>
 		<a className='center block underline' href='/forgot-password'>I don't remember my password</a>
 		<div className='line'></div>
-		<div className="tb-margin">
+		<div className='tb-margin'>
 			<p className='not-member'>
-				If you're a member of TorontoJS and don't have your account, <a href='/path-to-other-page' className="underline">click here to sign-up</a>
+				If you're a member of TorontoJS and don't have your account, <a href='/path-to-other-page' className='underline'>click here to sign-up</a>
 			</p>
 		</div>
 	</form>
