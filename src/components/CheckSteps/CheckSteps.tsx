0b95744ef17b671b878fffa6cbb31064abd7a21a
Lyla <mjuudev@gmail.com>
2025-04-03 23:02:12 -0400

    style: centralize components
    Co-authored-by: sounmind <devleesche@gmail.com>

--- src/components/CheckSteps/CheckSteps.tsx
+++ src/components/CheckSteps/CheckSteps.tsx
@@ -4,26 +4,26 @@ import Button from '../Button/Button';
 import Header from '../Header/Header';
 import './CheckSteps.css';
 
-const CheckSteps = () => {
-	return (
-		<div className='App'>
+const CheckSteps = () => (
+	<div className='check-steps-wrapper'>
+		<div className='check-steps'>
 			<Header>Welcome to TorontoJS Community Hub!</Header>
 			<p className='intro'>Your account is active, and you're almost ready to get in. Let's complete a few more steps:</p>
 			<ul className='cards'>
 				<li className='card'>
 					<CheckConductCode />
-					<p>Check the TorontoJS's conduct code</p>
+					<p className='text-h6'>1. Check the TorontoJS's conduct code</p>
 				</li>
 				<li className='card'>
 					<CompleteProfile />
-					<p>Complete your profile</p>
+					<p className='text-h6'>2. Complete your profile</p>
 				</li>
 			</ul>
 			<Button isPrimary isLarge>
 				Let's continue
 			</Button>
 		</div>
-	);
-};
+	</div>
+);
 
 export default CheckSteps;
