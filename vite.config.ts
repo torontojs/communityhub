e191aad36deace999c02ac33f0bbb3750f3bde50
Lyla <mjuudev@gmail.com>
2025-04-07 15:44:18 -0400

    build: add pages

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,9 @@ export default defineConfig(({ mode }) => {
 					'index': fileURLToPath(new URL('./src/index.html', import.meta.url)),
 					'profile': fileURLToPath(new URL('./src/pages/profile/index.html', import.meta.url)),
 					'team': fileURLToPath(new URL('./src/pages/team/index.html', import.meta.url)),
-					'complete-profile': fileURLToPath(new URL('./src/pages/complete-profile/index.html', import.meta.url))
+					'complete-profile': fileURLToPath(new URL('./src/pages/complete-profile/index.html', import.meta.url)),
+					'check-steps': fileURLToPath(new URL('./src/pages/check-steps/index.html', import.meta.url)),
+					'review-conduct-code': fileURLToPath(new URL('./src/pages/review-conduct-code/index.html', import.meta.url))
 				}
 			}
 		},
