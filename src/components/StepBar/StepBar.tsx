7113332cd3ee123319dace25c53c691851579239
Lyla <mjuudev@gmail.com>
2025-04-11 11:28:14 -0400

    fix: add nav tag and remove state in StepBar

--- src/components/StepBar/StepBar.tsx
+++ src/components/StepBar/StepBar.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { CheckIcon } from './icons/CheckIcon';
 import './StepBar.css';
 
@@ -7,33 +6,25 @@ interface Props {
 }
 
 const StepBar = ({ step }: Props) => {
-	const [width, setWidth] = useState(window.innerWidth);
-	const isMobile = width <= 640;
-
-	useEffect(() => {
-		const handleResize = () => setWidth(window.innerWidth);
-		window.addEventListener('resize', handleResize);
-
-		return () => window.removeEventListener('resize', handleResize);
-	}, []);
-
 	return (
-		<ol className='step-bar'>
-			<li className={`step-item ${step === 1 ? 'current' : ''} inactive done`}>
-				<span className='step-number'>
-					<CheckIcon />
-				</span>
-				{!isMobile || step === 1 ? <span className='step-text'>Account confirmed</span> : ''}
-			</li>
-			<li className={`step-item ${step === 2 ? 'current active' : 'inactive'} ${step > 2 ? 'done' : ''}`}>
-				<span className='step-number'>{step > 2 ? <CheckIcon /> : '2'}</span>
-				{!isMobile || step === 2 ? <span className='step-text'>Check the conduct code</span> : ''}
-			</li>
-			<li className={`step-item ${step === 3 ? 'current active' : 'inactive'}`}>
-				<span className='step-number'>3</span>
-				{!isMobile || step === 3 ? <span className='step-text'>Complete your profile</span> : ''}
-			</li>
-		</ol>
+		<nav aria-label='registration-steps'>
+			<ol className='step-list'>
+				<li className={`step-item ${step === 1 ? 'current' : ''} inactive done`}>
+					<span className='step-number'>
+						<CheckIcon />
+					</span>
+					<span className='step-text'>Account confirmed</span>
+				</li>
+				<li className={`step-item ${step === 2 ? 'current active' : 'inactive '} ${step > 2 ? 'done' : ''}`}>
+					<span className='step-number'>{step > 2 ? <CheckIcon /> : '2'}</span>
+					<span className='step-text'>Check the conduct code</span>
+				</li>
+				<li className={`step-item ${step === 3 ? 'current active' : 'inactive '}`}>
+					<span className='step-number'>3</span>
+					<span className='step-text'>Complete your profile</span>
+				</li>
+			</ol>
+		</nav>
 	);
 };
 
