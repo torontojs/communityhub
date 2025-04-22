c5a3b0bd6d2b7fe8e38b612fc0ce0193f74eb9ef
kbventures <kmb_x@hotmail.com>
2025-04-22 15:32:08 -0400

    fix added sign-up to vite build pages steps

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig(({ mode }) => {
 					'team': fileURLToPath(new URL('./src/pages/team/index.html', import.meta.url)),
 					'complete-profile': fileURLToPath(new URL('./src/pages/complete-profile/index.html', import.meta.url)),
 					'check-steps': fileURLToPath(new URL('./src/pages/check-steps/index.html', import.meta.url)),
-					'review-conduct-code': fileURLToPath(new URL('./src/pages/review-conduct-code/index.html', import.meta.url)),	
+					'review-conduct-code': fileURLToPath(new URL('./src/pages/review-conduct-code/index.html', import.meta.url)),
 					'sign-up': fileURLToPath(new URL('./src/pages/sign-in/index.html', import.meta.url))
 				}
 			}
