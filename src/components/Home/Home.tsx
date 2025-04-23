be2145edd7bedbdcedb6cff82ad776656a0639ee
kbventures <kmb_x@hotmail.com>
2025-04-23 17:09:37 -0400

    Home Page Draft v0

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,33 +1,33 @@
 import '../../components/Home/Home.css';
-import { useEffect, useState } from 'react'; 
+import { useEffect, useState } from 'react';
 
-const [ authenticated, setAuthenticated ] = useState<boolean>(false)
+const [authenticated, setAuthenticated] = useState<boolean>(false);
 
-useEffect(()=>{
-    const fetchHeartBeat =  async () =>{
-        try {
-            const response = await fetch('')
-            const data = await response.json()
-            if(data.ok){
-                setAuthenticated(true)
-            } else {
-                window.location.href = 'sign-in'
-            }
-        } catch (err){
-            throw new Error; 
+useEffect(() => {
+	const fetchHeartBeat = async () => {
+		try {
+			const response = await fetch('');
+			const data = await response.json();
+			if (data.ok) {
+				setAuthenticated(true);
+			} else {
+				window.location.href = 'sign-in';
+			}
+		} catch (err) {
+			throw new Error();
+		}
+	};
 
-        }
-    }
+	const isAuthenticated = fetchHeartBeat();
+}, []);
 
-    const isAuthenticated = fetchHeartBeat()
-}, [])
+const handleLogOut = () => console.log('haha');
 
-const handleLogOut = () => console.log('haha')
-
-export const Home = () => 
-<>
-<nav>
-    <button type='button' onClick={handleLogOut}></button>
-</nav>
-<div>Is Authenticated: {authenticated}</div>;
-</>
+export const Home = () => (
+	<>
+		<nav>
+			<button type='button' onClick={handleLogOut}></button>
+		</nav>
+		<div>Is Authenticated: {authenticated}</div>;
+	</>
+);
