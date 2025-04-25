1fb29160b2f25f34e815f78f93f692b7e6d58340
kbventures <kmb_x@hotmail.com>
2025-04-25 15:17:31 -0400

    Fix remove home because working on it in another branch

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,6 @@ export default defineConfig(({ mode }) => {
 					'review-conduct-code': fileURLToPath(new URL('./src/pages/review-conduct-code/index.html', import.meta.url)),
 					'sign-up': fileURLToPath(new URL('./src/pages/sign-up/index.html', import.meta.url)),
 					'sign-in': fileURLToPath(new URL('./src/pages/sign-in/index.html', import.meta.url)),
-					'home': fileURLToPath(new URL('./src/pages/home/index.html', import.meta.url))
 				}
 			}
 		},
