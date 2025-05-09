b323f0dfcf812bf8708fdfdf24527bdd1adaf343
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-05-09 02:50:53 -0400

    style: update Button component styles
    Update Button component styles for updated design changes.
    Add a ref prop to the Button component.
    Create a separate file for showing sample buttons.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import type { ButtonHTMLAttributes } from 'react';
+import { type ButtonHTMLAttributes, forwardRef } from 'react';
 import './Button.css';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -7,19 +7,20 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 	isLarge?: boolean;
 }
 
-const Button = ({
+const Button = forwardRef<HTMLButtonElement, Props>(({
 	hasOutline = false,
 	isPrimary = false,
 	isLarge = false,
 	children,
 	...rest
-}: Props) => (
+}, ref) => (
 	<button
+		ref={ref}
 		className={`button ${isPrimary ? 'primary' : ''} ${hasOutline ? 'outline' : ''} ${isLarge ? 'large' : 'small'}`}
 		{...rest}
 	>
 		{children}
 	</button>
-);
+));
 
 export default Button;
