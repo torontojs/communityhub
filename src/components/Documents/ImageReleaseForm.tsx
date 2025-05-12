4010bd7e246625902e6d16a919081001d81e052b
madcampos <me@madcampos.dev>
2025-05-11 22:15:39 -0400

    feat: add component for documents

---
+++ src/components/Documents/ImageReleaseForm.tsx
@@ -0,0 +1,9 @@
+export const ImageReleaseForm = () => (
+	<>
+		<p>
+			The volunteer authorizes the organization to photograph and/or record their image and/or voice and to use this material to promote Toronto JS, its activities and its
+			speakers through the media of newsletters, websites, television, film, radio, print and/or display form. The volunteer consents that the materials and copyright will
+			remain the sole property of the organization.
+		</p>
+	</>
+);
