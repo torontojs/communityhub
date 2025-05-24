b47150d7145cee4c9292c7eb98f2c0da4e3f377a
madcampos <me@madcampos.dev>
2025-05-24 19:57:21 -0400

    chore: stope eslint from complaining about async code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -54,7 +54,7 @@ const rules = {
 	'no-alert': 'error',
 	'no-array-constructor': 'off',
 	'no-async-promise-executor': 'error',
-	'no-await-in-loop': 'error',
+	'no-await-in-loop': 'off',
 	'no-bitwise': 'error',
 	'no-caller': 'error',
 	'no-case-declarations': 'error',
