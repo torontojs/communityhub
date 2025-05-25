006f1d7c22b1fdcaf8f9e7598b3bb8bf5eed61e2
madcampos <me@madcampos.dev>
2025-05-24 20:04:56 -0400

    feat: add scheduler handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,6 +10,7 @@ import { healthCheckRoutes } from './routes/health-check/index.ts';
 import { profileRoutes } from './routes/profile/index.ts';
 import { teamMemberRoutes } from './routes/team-members/index.ts';
 import { teamRoutes } from './routes/team/index.ts';
+import { SchedulerHandler } from './scheduler/index.ts';
 import { StatusCodes, statusResponseFormatter } from './utils/responses.ts';
 
 const app = new OpenAPIHono<EnvironmentBindings>({
@@ -82,4 +83,7 @@ apiRoutes.route('/teams', teamMemberRoutes);
 // Make all routes prefixed to /api
 app.route('/api', apiRoutes);
 
-export default app;
+export default {
+	fetch: app.fetch,
+	scheduled: new SchedulerHandler(app).handler
+};
