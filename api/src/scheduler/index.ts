9259ee34637d1668b577134846362d95dd780242
madcampos <me@madcampos.dev>
2025-05-24 20:35:02 -0400

    fix: move to functional code

--- api/src/scheduler/index.ts
+++ api/src/scheduler/index.ts
@@ -1,27 +1,21 @@
 import type { OpenAPIHono } from '@hono/zod-openapi';
 import { exportData } from './data-export.ts';
 
-export class SchedulerHandler {
-	// Cron expression tester/references:
-	// https://crontab.cronhub.io/
-	// https://developers.cloudflare.com/workers/configuration/cron-triggers/#supported-cron-expressions
-	#cronExpressions = {
-		everyDayAt02am: '0 2 * * *'
-	} as const;
+// Cron expression tester/references:
+// https://crontab.cronhub.io/
+// https://developers.cloudflare.com/workers/configuration/cron-triggers/#supported-cron-expressions
+const cronExpressions = {
+	everyDayAt02am: '0 2 * * *'
+} as const;
 
-	#app: OpenAPIHono<EnvironmentBindings>;
-
-	constructor(app: OpenAPIHono<EnvironmentBindings>) {
-		this.#app = app;
-	}
-
-	async handler(controller: ScheduledController, env: Env, context: ExecutionContext) {
+export function cronHandler(app: OpenAPIHono<EnvironmentBindings>) {
+	return async (controller: ScheduledController, env: Env, context: ExecutionContext) => {
 		switch (controller.cron) {
-			case this.#cronExpressions.everyDayAt02am:
-				await exportData(this.#app, env, context);
+			case cronExpressions.everyDayAt02am:
+				await exportData(app, env, context);
 				break;
 			default:
 				break;
 		}
-	}
+	};
 }
