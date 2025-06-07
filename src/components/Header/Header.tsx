fb191cfe5b9186b015ff0f3ce975a0d9fd362884
Ekep Obasi <arreyettaekep@gmail.com>
2025-06-07 08:25:41 -0400

    chore(ui): centralize page layout and enhance header component customization
    - Updated  to centralize the page layout for better alignment.
    - Extended  and  components to allow more customization via props.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import type { ComponentProps, ReactNode } from 'react';
 import HeaderTitle from '../HeaderTitle/HeaderTitle.tsx';
 import Logo from '../Logo/Logo.tsx';
 import './Header.css';
@@ -6,12 +6,13 @@ import './Header.css';
 interface Props {
 	children: ReactNode;
 	isLogoLarge?: boolean;
+	textProps?: ComponentProps<'h1'>;
 }
 
-const Header = ({ children, isLogoLarge = true }: Props) => (
+const Header = ({ children, isLogoLarge = true, textProps }: Props) => (
 	<header id='header-with-logo'>
 		<Logo isLarge={isLogoLarge} />
-		<HeaderTitle>{children}</HeaderTitle>
+		<HeaderTitle {...textProps}>{children}</HeaderTitle>
 	</header>
 );
 
