7f913e2be9526d65b29c24e0523e33c4cdad2802
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-07-22 11:33:50 -0400

    fix: update helper message to use svg defs

--- src/components/HelperMessageComponent/HelperMessageComponent.tsx
+++ src/components/HelperMessageComponent/HelperMessageComponent.tsx
@@ -7,10 +7,20 @@ interface Props {
 	labelText: string;
 }
 
+const iconMap: Record<HelperMessageComponentVariants, string> = {
+	default: 'icon-default',
+	info: 'icon-info',
+	success: 'icon-success',
+	warning: 'icon-warning',
+	error: 'icon-error'
+};
+
 const HelperMessageComponent = ({ variant = 'default', labelText }: Props) => {
 	return (
 		<div className='helper-container' data-variant={variant}>
-			<span className='helper-icon' />
+			<svg className='helper-icon'>
+				<use href={`#${iconMap[variant]}`} />
+			</svg>
 			<span className='helper-content'>{labelText}</span>
 		</div>
 	);
