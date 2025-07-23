21211615becde2a37e2c1c77a025a40f2be14218
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-07-23 11:02:02 -0400

    fix: update html and css for semantics
    replace h3 html tags with fieldset and legend.
    update css class name.
    remove non-required css.
    update heading hierarchy.

--- src/pages/usage-helper-message/sample-helper-message.tsx
+++ src/pages/usage-helper-message/sample-helper-message.tsx
@@ -18,62 +18,73 @@ createRoot(root).render(
 			<div className='App'>
 				{/* NOTE: This is temp UI to test the helper message usage with text input or other components */}
 
-				<h2 style={{ textAlign: 'center' }}>Render the Design System's Helper Message Input Component Cases</h2>
+				<h1>Render the Design System's Helper Message Input Component Cases</h1>
 				<p>
 					<em>IMPORTANT:</em> Must import the SVG Definitions Component - HelperMessageSvgDefinitions, to use the helper message
 				</p>
 
-				<div className='component-container'>
-					<h3>
-						Default Variant:
-					</h3>
-					<HelperMessageComponent variant='default' labelText='Helper Message' />
-
-					<h3>
-						Error Variant:
-					</h3>
-					<HelperMessageComponent variant='error' labelText='Helper Message' />
-
-					<h3>
-						Info Variant:
-					</h3>
-					<HelperMessageComponent variant='info' labelText='Helper Message' />
-
-					<h3>
-						Success Variant:
-					</h3>
-					<HelperMessageComponent variant='success' labelText='Helper Message' />
-
-					<h3>
-						Warning Variant:
-					</h3>
-					<HelperMessageComponent variant='warning' labelText='Helper Message' />
-
-					<h3>
-						Usage with Input Component: Default
-					</h3>
-					<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent labelText='Helper label' />} />
-
-					<h3>
-						Usage with Input Component: Error
-					</h3>
-					<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='error' labelText='Helper label' />} />
-
-					<h3>
-						Usage with Input Component: Info
-					</h3>
-					<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='info' labelText='Helper label' />} />
-
-					<h3>
-						Usage with Input Component: Success
-					</h3>
-					<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='success' labelText='Helper label' />} />
-
-					<h3>
-						Usage with Input Component: Warning
-					</h3>
-					<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='warning' labelText='Helper label' />} />
-				</div>
+				<section className='helper-message-showcase-container'>
+					<fieldset>
+						<legend>
+							Default Variant:
+						</legend>
+						<HelperMessageComponent variant='default' labelText='Helper Message' />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Error Variant:
+						</legend>
+						<HelperMessageComponent variant='error' labelText='Helper Message' />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Info Variant:
+						</legend>
+						<HelperMessageComponent variant='info' labelText='Helper Message' />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Success Variant:
+						</legend>
+						<HelperMessageComponent variant='success' labelText='Helper Message' />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Warning Variant:
+						</legend>
+						<HelperMessageComponent variant='warning' labelText='Helper Message' />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Usage with Input Component: Default
+						</legend>
+						<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent labelText='Helper label' />} />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Usage with Input Component: Error
+						</legend>
+						<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='error' labelText='Helper label' />} />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Usage with Input Component: Info
+						</legend>
+						<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='info' labelText='Helper label' />} />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Usage with Input Component: Success
+						</legend>
+						<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='success' labelText='Helper label' />} />
+					</fieldset>
+					<fieldset>
+						<legend>
+							Usage with Input Component: Warning
+						</legend>
+						<TextInputComponent label='Label' helperMessageSlot={<HelperMessageComponent variant='warning' labelText='Helper label' />} />
+					</fieldset>
+				</section>
 			</div>
 		</StrictMode>
 	)
