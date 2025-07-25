e9b0e0f368a10127a9ea1e7a5adc8692a26cd364
madcampos <madcampos@outlook.com>
2025-07-25 11:56:15 -0400

    fix: file upload size and docs for size limits

--- api/src/middleware/body-size.ts
+++ api/src/middleware/body-size.ts
@@ -4,23 +4,23 @@ import { bodyLimit } from 'hono/body-limit';
 import { StatusCodes, type StatusResponse } from '../utils/responses.ts';
 
 /** 48kb */
-const TOTAL_FILE_SIZE_IN_KB = 48 * 1024;
+const TOTAL_FILE_SIZE_IN_BYTES = 48 * 1024;
 
-/** 512kb */
-const FILE_UPLOAD_SIZE_IN_KB = 512 * 1024;
+/** 5mb */
+const FILE_UPLOAD_SIZE_IN_BYTES = 5 * 1024 * 1024;
 
-/** 1024 characters (1024bytes) */
+/** 1kb (1024 characters) */
 export const SHORT_TEXT_SIZE_IN_CHAR = 1024;
 
-/** 16 * 1024 characters (16kb) */
+/** 16kb (16 * 1024 characters) */
 export const LONG_TEXT_SIZE_IN_CHAR = 16 * 1024;
 
 export const bodySizeCheck = bodyLimit({
-	maxSize: TOTAL_FILE_SIZE_IN_KB,
+	maxSize: TOTAL_FILE_SIZE_IN_BYTES,
 	onError: (context) => context.json({ message: 'Body size exceeded' } satisfies StatusResponse, StatusCodes.CONTENT_TOO_LARGE)
 });
 
 export const fileUploadSizeCheck = bodyLimit({
-	maxSize: FILE_UPLOAD_SIZE_IN_KB,
+	maxSize: FILE_UPLOAD_SIZE_IN_BYTES,
 	onError: (context) => context.json({ message: 'Body size exceeded' } satisfies StatusResponse, StatusCodes.CONTENT_TOO_LARGE)
 });
