401c82b16376affa6873cf7bb5428df455402f20
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-08-17 15:07:40 -0400

    refactor: update CheckSteps to use useProfileRedirect hook

--- src/components/CheckSteps/CheckSteps.tsx
+++ src/components/CheckSteps/CheckSteps.tsx
@@ -1,3 +1,4 @@
+import { useProfileRedirect } from '../../hooks/useProfileRedirect.ts';
 import ButtonLink from '../ButtonLink/ButtonLink.tsx';
 import Header from '../Header/Header.tsx';
 import { CheckConductCodeIcon } from '../Icons/CheckConductCodeIcon.tsx';
@@ -5,27 +6,35 @@ import { CompleteProfileIcon } from '../Icons/CompleteProfileIcon.tsx';
 import StepBar from '../StepBar/StepBar.tsx';
 import './CheckSteps.css';
 
-const CheckSteps = () => (
-	<div id='check-steps-wrapper'>
-		<StepBar currentStep={1} steps={[{ label: 'Account confirmed' }, { label: 'Check the conduct code' }, { label: 'Complete your profile' }]} />
-		<div id='check-steps'>
-			<Header isLogoLarge={false}>Welcome to TorontoJS Community Hub!</Header>
-			<p className='intro'>Your account is active, and you're almost ready to get in. Let's complete a few more steps:</p>
-			<ul className='cards'>
-				<li className='card'>
-					<CheckConductCodeIcon />
-					<p className='text-h6'>Check the TorontoJS's conduct code</p>
-				</li>
-				<li className='card'>
-					<CompleteProfileIcon />
-					<p className='text-h6'>Complete your profile</p>
-				</li>
-			</ul>
-			<ButtonLink id='continue-button' isPrimary isLarge href='/pages/review-conduct-code/'>
-				Let's continue
-			</ButtonLink>
-		</div>
-	</div>
-);
+const CheckSteps = () => {
+	const { redirectionComplete } = useProfileRedirect();
+
+	if (redirectionComplete) {
+		return (
+			<div id='check-steps-wrapper'>
+				<StepBar currentStep={1} steps={[{ label: 'Account confirmed' }, { label: 'Check the conduct code' }, { label: 'Complete your profile' }]} />
+				<div id='check-steps'>
+					<Header isLogoLarge={false}>Welcome to TorontoJS Community Hub!</Header>
+					<p className='intro'>Your account is active, and you're almost ready to get in. Let's complete a few more steps:</p>
+					<ul className='cards'>
+						<li className='card'>
+							<CheckConductCodeIcon />
+							<p className='text-h6'>Check the TorontoJS's conduct code</p>
+						</li>
+						<li className='card'>
+							<CompleteProfileIcon />
+							<p className='text-h6'>Complete your profile</p>
+						</li>
+					</ul>
+					<ButtonLink id='continue-button' isPrimary isLarge href='/pages/review-conduct-code/'>
+						Let's continue
+					</ButtonLink>
+				</div>
+			</div>
+		);
+	}
+
+	return <div>loading...</div>;
+};
 
 export default CheckSteps;
