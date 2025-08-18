6eec33c2d5665eaf31793837285e7b63d73fa62a
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-08-18 19:16:02 -0400

    fix: add early return to CheckSteps

--- src/components/CheckSteps/CheckSteps.tsx
+++ src/components/CheckSteps/CheckSteps.tsx
@@ -9,32 +9,32 @@ import './CheckSteps.css';
 const CheckSteps = () => {
 	const { redirectionComplete } = useProfileRedirect();
 
-	if (redirectionComplete) {
-		return (
-			<div id='check-steps-wrapper'>
-				<StepBar currentStep={1} steps={[{ label: 'Account confirmed' }, { label: 'Check the conduct code' }, { label: 'Complete your profile' }]} />
-				<div id='check-steps'>
-					<Header isLogoLarge={false}>Welcome to TorontoJS Community Hub!</Header>
-					<p className='intro'>Your account is active, and you're almost ready to get in. Let's complete a few more steps:</p>
-					<ul className='cards'>
-						<li className='card'>
-							<CheckConductCodeIcon />
-							<p className='text-h6'>Check the TorontoJS's conduct code</p>
-						</li>
-						<li className='card'>
-							<CompleteProfileIcon />
-							<p className='text-h6'>Complete your profile</p>
-						</li>
-					</ul>
-					<ButtonLink id='continue-button' isPrimary isLarge href='/pages/review-conduct-code/'>
-						Let's continue
-					</ButtonLink>
-				</div>
-			</div>
-		);
+	if (!redirectionComplete) {
+		return <div>loading...</div>;
 	}
 
-	return <div>loading...</div>;
+	return (
+		<div id='check-steps-wrapper'>
+			<StepBar currentStep={1} steps={[{ label: 'Account confirmed' }, { label: 'Check the conduct code' }, { label: 'Complete your profile' }]} />
+			<div id='check-steps'>
+				<Header isLogoLarge={false}>Welcome to TorontoJS Community Hub!</Header>
+				<p className='intro'>Your account is active, and you're almost ready to get in. Let's complete a few more steps:</p>
+				<ul className='cards'>
+					<li className='card'>
+						<CheckConductCodeIcon />
+						<p className='text-h6'>Check the TorontoJS's conduct code</p>
+					</li>
+					<li className='card'>
+						<CompleteProfileIcon />
+						<p className='text-h6'>Complete your profile</p>
+					</li>
+				</ul>
+				<ButtonLink id='continue-button' isPrimary isLarge href='/pages/review-conduct-code/'>
+					Let's continue
+				</ButtonLink>
+			</div>
+		</div>
+	);
 };
 
 export default CheckSteps;
