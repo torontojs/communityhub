2b58ef2d072de39e0d5d0f2ba0cbbb32d2cb8de0
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-10-05 16:38:56 -0400

    refactor: store avatar url in profile table directly
    update avatar url in the profile table instead of creating a separate uploads table.
    delete uploads table.

--- api/src/routes/files/data.ts
+++ api/src/routes/files/data.ts
@@ -1,40 +1,17 @@
-import { DBTables, SCHEMA_VERSION } from '../../utils/db.ts';
-import type { AllowedFileMimeType, FileAccessLevelType, UploadedFileType } from './validation.ts';
+import { DBTables } from '../../utils/db.ts';
 
-export const fetchFileInfo = async (database: D1Database, fileNameAsId: string, fileAccessLevel: FileAccessLevelType = 'public', isAuthenticated = false) => {
-	// Search all files if requesting protected file and authenticated, otherwise only search public files
-	const accessLevelCheck = fileAccessLevel === 'protected' && isAuthenticated ? '' : "AND accessLevel = 'public'";
-	const dbQuery = `
-		SELECT id AS fileName, mimeType, accessLevel
-		FROM ${DBTables.UPLOADS}
-		WHERE
-			id = ?
-			${accessLevelCheck}
-		LIMIT 1
-	`;
-
-	const fileInfo = await database
-		.prepare(dbQuery)
-		.bind(fileNameAsId)
-		.first<{ fileName: string, mimeType: AllowedFileMimeType, accessLevel: FileAccessLevelType }>();
-
-	return fileInfo;
-};
-
-export const uploadFile = async (
+export const updateAvatar = async (
 	database: D1Database,
 	profileId: string,
-	fileNameAsId: string,
-	fileType: UploadedFileType,
-	fileMimeType: AllowedFileMimeType,
-	accessLevel: FileAccessLevelType = 'public'
+	fileName: string
 ) => {
-	const now = new Date().toISOString();
-
 	const { success } = await database.prepare(`
-		INSERT INTO ${DBTables.UPLOADS} (id, schemaVersion, profileId, type,  insertedAt, mimeType, accessLevel) VALUES (?, ?, ?, ?, ?, ?, ?)
+		UPDATE ${DBTables.PROFILE}
+			SET avatar = ?
+			WHERE id = ?
+			LIMIT 1
 	`)
-		.bind(fileNameAsId, SCHEMA_VERSION, profileId, fileType, now, fileMimeType, accessLevel)
+		.bind(fileName, profileId)
 		.run();
 
 	return success;
