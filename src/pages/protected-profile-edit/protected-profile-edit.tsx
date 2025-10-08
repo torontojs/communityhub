63cf7e03c09e276ba3b3048a0d0b06b52f0abcda
kbventures <kmb_x@hotmail.com>
2025-10-08 16:12:54 -0400

    Updated AuthGate in protected profile page

--- src/pages/protected-profile-edit/protected-profile-edit.tsx
+++ src/pages/protected-profile-edit/protected-profile-edit.tsx
@@ -4,16 +4,17 @@ import { createRoot } from 'react-dom/client';
 import 'open-props';
 import '../../index.css';
 import './style.css';
-import { AuthGateProtected } from '../../components/AuthGate/AuthGate.tsx';
+import { AuthGate } from '../../components/AuthGate/AuthGate.tsx';
 import { ProtectedProfileEdit } from '../../components/ProtectedProfileEdit/ProtectedProfileEdit.tsx';
+import { useHeartBeatProtected } from '../../hooks/useHeartBeat.ts';
 const root = document.getElementById('root') as HTMLDivElement;
 
 createRoot(root).render(
 	(
 		<StrictMode>
-			<AuthGateProtected>
+			<AuthGate hook={useHeartBeatProtected}>
 				<ProtectedProfileEdit />
-			</AuthGateProtected>
+			</AuthGate>
 		</StrictMode>
 	)
 );
--- src/pages/protected-profile/protected-profile.tsx
+++ src/pages/protected-profile/protected-profile.tsx
@@ -4,16 +4,17 @@ import { createRoot } from 'react-dom/client';
 import 'open-props';
 import '../../index.css';
 import './style.css';
-import { AuthGateProtected } from '../../components/AuthGate/AuthGate.tsx';
+import { AuthGate } from '../../components/AuthGate/AuthGate.tsx';
 import { ProtectedProfile } from '../../components/ProtectedProfile/ProtectedProfile.tsx';
+import { useHeartBeatProtected } from '../../hooks/useHeartBeat.ts';
 const root = document.getElementById('root') as HTMLDivElement;
 
 createRoot(root).render(
 	(
 		<StrictMode>
-			<AuthGateProtected>
+			<AuthGate hook={useHeartBeatProtected}>
 				<ProtectedProfile />
-			</AuthGateProtected>
+			</AuthGate>
 		</StrictMode>
 	)
 );
