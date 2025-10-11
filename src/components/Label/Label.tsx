bc2dd14260292ee4b03cb270cdde6ca2031ddaef
Roshan Budhathoki <budhathoki.roshan321@gmail.com>
2025-10-11 10:16:05 -0400

    udate: change in label props and usage design

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,18 +1,48 @@
-import { forwardRef, type ParamHTMLAttributes } from 'react';
+import React, { forwardRef, type ParamHTMLAttributes } from 'react';
 import './Label.css';
 
-interface Props extends ParamHTMLAttributes<HTMLParagraphElement> {
-	isPrimary?: boolean;
+type LabelColor =
+	| 'accent'
+	| 'default'
+	| 'error'
+	| 'warning'
+	| 'success'
+	| 'info';
+
+type LabelComponentTones = 'dark' | 'light';
+
+interface Props extends ParamHTMLAttributes<HTMLSpanElement> {
+	variant?: LabelColor;
+	tone?: LabelComponentTones;
+	hasIcon?: boolean;
+	icon?: React.JSX.Element;
 }
 
-const Label = forwardRef<HTMLParagraphElement, Props>(({
-	isPrimary = false,
+const Label = forwardRef<HTMLSpanElement, Props>(({
+	variant = 'default',
+	tone = 'dark',
+	hasIcon,
+	icon,
 	children,
 	...rest
-}, ref) => (
-	<p ref={ref} className='label' {...rest} data-primary={isPrimary}>
+}) => (
+	<span className='detail-tab' {...rest} data-variant={variant} data-tone={tone} data-icon={hasIcon}>
+		{hasIcon && !icon &&
+			(
+				<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'>
+					<g clip-path='url(#clip0_3317_7306)'>
+						<path d='M13.3334 4L6.00008 11.3333L2.66675 8' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+					</g>
+					<defs>
+						<clipPath id='clip0_3317_7306'>
+							<rect width='16' height='16' fill='currentColor' />
+						</clipPath>
+					</defs>
+				</svg>
+			)}
+		{icon}
 		{children}
-	</p>
+	</span>
 ));
 
 export default Label;
--- src/pages/label-usage/sample-label.tsx
+++ src/pages/label-usage/sample-label.tsx
@@ -10,10 +10,64 @@ const root = document.getElementById('root') as HTMLDivElement;
 createRoot(root).render(
 	(
 		<StrictMode>
-			<div className='App parent'>
+			<div className='parent'>
 				<h2>Label Usage</h2>
-				<Label>Default</Label>
-				<Label isPrimary>Primary</Label>
+				<div className='label-collection'>
+					<div>
+						<p>Accent Dark</p>
+						<Label variant='accent' hasIcon>Label</Label>
+					</div>
+					<div>
+						<p>Accent Light</p>
+						<Label variant='accent' tone='light' hasIcon>Label</Label>
+					</div>
+					<div>
+						<p>Error Dark</p>
+						<Label variant='error' hasIcon>Error</Label>
+					</div>
+					<div>
+						<p>Error Light</p>
+						<Label variant='error' tone='light' hasIcon>Error</Label>
+					</div>
+				</div>
+
+				<div className='label-collection'>
+					<div>
+						<p>Warning Dark</p>
+						<Label variant='warning' hasIcon>Warning</Label>
+					</div>
+					<div>
+						<p>Warning Light</p>
+						<Label variant='warning' tone='light' hasIcon>Warning</Label>
+					</div>
+					<div>
+						<p>Success Dark</p>
+						<Label variant='success' hasIcon>Success</Label>
+					</div>
+					<div>
+						<p>Success Light</p>
+						<Label variant='success' tone='light' hasIcon>Success</Label>
+					</div>
+				</div>
+
+				<div className='label-collection'>
+					<div>
+						<p>Info Dark</p>
+						<Label variant='info' hasIcon>Info</Label>
+					</div>
+					<div>
+						<p>Info Light</p>
+						<Label variant='info' tone='light' hasIcon>Info</Label>
+					</div>
+					<div>
+						<p>Default Dark</p>
+						<Label hasIcon>Default</Label>
+					</div>
+					<div>
+						<p>Default Light</p>
+						<Label tone='light' hasIcon>Default</Label>
+					</div>
+				</div>
 			</div>
 		</StrictMode>
 	)
