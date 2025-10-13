89129924e0f1f2520d0c0a5ff0d45ee477c5533f
Ualnomis <ualnamnuhc@gmail.com>
2025-10-13 17:36:50 -0400

    fix: `vitest watch` fails on the second run
    references:
    - https://github.com/cloudflare/workers-sdk/issues/9913
    - https://github.com/cloudflare/workers-sdk/issues/10506

--- api/src/global.d.ts
+++ api/src/global.d.ts
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 
-interface EnvironmentBindings {
+export interface EnvironmentBindings {
 	Bindings: Cloudflare.Env;
 
 	Variables: {
 		session: import('./utils/auth.ts').SessionData
 	};
+}
+
+declare module 'cloudflare:test' {
+	// ProvidedEnv controls the type of `import("cloudflare:test").env`
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
+	interface ProvidedEnv extends Env {}
 }
 
 type ISODate = string;
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,11 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
 	test: {
-		globals: true,
-		coverage: { reporter: ['text', 'lcov'] },
 		poolOptions: {
 			workers: {
 				wrangler: { configPath: './wrangler.toml' }
 			}
-		}
+		},
+		coverage: { reporter: ['text', 'lcov'] }
 	}
 });
