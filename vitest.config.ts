70b82e0060117f4240a61e0d0f01dbaacaf43da3
Ualnomis <ualnamnuhc@gmail.com>
2025-10-14 11:41:16 -0400

    chore: update each SQL statement to single line for `vitest-pool-workers`

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,9 @@ export default defineWorkersConfig(async () => {
 	const migrationsPath = path.join(process.cwd(), 'db/migrations');
 	const migrations = await readD1Migrations(migrationsPath);
 
-	const seedSql = fs.readFileSync(path.join(process.cwd(), 'db/seeds/seed-data-no-comment.sql'), 'utf8');
+	const rawSeedSql = fs.readFileSync(path.join(process.cwd(), 'db/seeds/seed-data.sql'), 'utf8');
+	const seedSql = rawSeedSql.split('\n').filter(line => !line.trim().startsWith('--')).join("\n").replace(/\n/g, " ");
+
 	return {
 		test: {
 			poolOptions: {
