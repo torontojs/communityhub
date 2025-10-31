a32fb9cbcb6bcf1bd22bbff97b46df9bcb067d7a
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-10-30 21:43:01 -0400

    Fix:Added style sheet for selectbox

--- src/components/SelectBox/SelectBoxComponent.tsx
+++ src/components/SelectBox/SelectBoxComponent.tsx
@@ -1,23 +1,56 @@
-import { forwardRef, type OptionHTMLAttributes } from 'react';
+import { forwardRef, type OptionHTMLAttributes } from "react";
+import "./SelectBoxComponent.css";
 
 interface Option {
-	label: string;
-	value: string;
+  label: string;
+  value: string;
 }
 interface SelectBoxProps extends OptionHTMLAttributes<HTMLSelectElement> {
-	placeholder?: string;
-	options: Option[];
-	label: string;
+  placeholder?: string;
+  options: Option[];
+  label: string;
+  helper?: string;
+  error?: string;
 }
 const SelectBoxComponent = forwardRef<HTMLSelectElement, SelectBoxProps>(
-	({ placeholder, options, label, value, disabled = false }, ref) => (
-		<div>
-			<label>{label}</label>
-			<select disabled={disabled} value={value} ref={ref}>
-				{placeholder && <option value=''>{placeholder}</option>}
-				{options.map((option) => <option value={option.value}>{option.label}</option>)}
-			</select>
-		</div>
-	)
+  (
+    {
+      placeholder,
+      options,
+      label,
+      value,
+      disabled = false,
+      helper = "",
+      error = "",
+      ...rest
+    },
+    ref
+  ) => (
+    <div
+      className={`selectbox-component-container ${disabled ? disabled : ""}`}
+    >
+      <span className="label-container">
+        <label>{label}</label>
+      </span>
+
+      <select
+        className="selectbox-container"
+        disabled={disabled}
+        value={value}
+        ref={ref}
+        defaultValue={value ?? ""}
+        data-error={Boolean(error)}
+        data-helper={Boolean(helper)}
+        {...rest}
+      >
+        {placeholder && <option value="">{placeholder}</option>}
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
 );
 export default SelectBoxComponent;
---
+++ src/pages/selectbox-usage/sample-selectbox.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import SelectBoxComponent from "../../components/SelectBox/SelectBoxComponent.tsx";
+
+
+const root=document.getElementById('root') as HTMLDivElement;
+
+interface Option {
+	label: string;
+	value: string;
+}
+const options:Option[]=[
+   { label: "Option 1", value: "value1" },
+  { label: "Option 2", value: "value2" },
+  { label: "Option 3", value: "value3" },
+]
+createRoot(root).render(
+    <StrictMode>
+        <div>
+            <SelectBoxComponent label='Country' options={options} value={"value1"} placeholder='Please select Value' />
+            <SelectBoxComponent label='Label' options={options} placeholder='Please select Value' disabled={true} />
+             <SelectBoxComponent label='Label' options={options} placeholder='Please select Value' error={'An error occurred'}  />
+             <SelectBoxComponent label='Label' options={options} placeholder='Please select Value' required/>
+        </div>
+    </StrictMode>
+)
