5149c9bff4c950f7e6b89291c2f13c479478926b
kbventures <kmb_x@hotmail.com>
2026-02-04 10:58:55 -0500

    Fixed social icon css error and added link to profile page in home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,5 +7,10 @@ export const Home = () => (
 			<button type='button' onClick={handleLogOut}>Log Out button</button>
 		</nav>
 		<h1>Home Page</h1>
+		<ul>
+			<li>
+				<a href='/pages/protected-profile'>Profile page</a>
+			</li>
+		</ul>
 	</>
 );
