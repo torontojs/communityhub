588d21128fe8ad1a5337d18e04a45282c5c120e6
Brandon Cha <bcha92@hotmail.com>
2026-03-27 14:13:29 -0400

    fix: pagination sql logic step 1/2

--- api/src/routes/team-members/data.ts
+++ api/src/routes/team-members/data.ts
@@ -93,7 +93,9 @@ export async function countAllMembers(database: D1Database, teamId: string) {
 	return results;
 }
 
-export async function getAllMembers(database: D1Database, teamId: string, limit?: number, offset = 0) {
+export async function getAllMembers(database: D1Database, teamId: string, limit = 0, offset = 0) {
+	const sqlLimit = limit;
+	const sqlOffset = offset;
 	const { results } = await database.prepare(`
 		SELECT
 			role.id AS id,
@@ -115,7 +117,7 @@ export async function getAllMembers(database: D1Database, teamId: string, limit?
 			AND role.deletedAt IS NULL
 			AND access.activatedAt IS NOT NULL
 			AND access.deletedAt IS NULL
-		${limit ? `LIMIT ${limit} OFFSET ${offset}` : ''}
+		${sqlLimit === 0 ? `LIMIT ${sqlLimit} OFFSET ${sqlOffset}` : ''}
 		`).bind(teamId).run<TeamMemberInfo>();
 
 	return results;
