958561b80d69ade411bf7d3e7ebb1ccbe39713bb
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-08-04 18:38:24 -0400

    Created the open_role and open_role_members in 0008_create-open-role.sql

--- db/migrations/0008_create-open-role.sql
+++ db/migrations/0008_create-open-role.sql
@@ -8,28 +8,28 @@ CREATE TABLE IF NOT EXISTS open_role(
 	-- The UUID, stored as text
 	id TEXT NOT NULL UNIQUE COLLATE BINARY,
 	-- The name of the open role
-	open_role_name TEXT NOT NULL,
+	openRoleName TEXT NOT NULL,
 	-- A text blurb the person can provide about themselves
-	open_role_description TEXT,
+	openRoleDescription TEXT,
 	-- A flag indicating if the user is based on the Grater Toronto Area (GTA)
 	-- This and the following flags are a proxy for information if people can attend online and in person events.
 	-- It is enough to give us information if the person is around Toronto without needing to ask the actual location.
 	isBasedOnGTA INTEGER NOT NULL DEFAULT 1 CHECK(isBasedOnGTA IN (0, 1)),
 	-- The experience level of the open role (senior, mid, entry)
-	experience_level TEXT NOT NULL CHECK(experience_level IN ('senior', 'mid', 'entry')),
+	experienceLevel TEXT NOT NULL CHECK(experienceLevel IN ('senior', 'mid', 'entry')),
 	-- The tech stacks required for the open role
-	tech_stacks TEXT NOT NULL,
+	techStacks TEXT NOT NULL,
 	-- The position of the open role (1,2,5...)
-	position_required INTEGER NOT NULL,
+	positionRequired INTEGER NOT NULL,
 	-- The team id of the open role (FK of Team(id))
-	team_id TEXT NOT NULL,
+	teamId TEXT NOT NULL,
 	-- The date this open role was added to the database, saved as an ISO timestamp
 	insertedAt DATETIME NOT NULL,
 	-- The date this open role was deleted from the database, saved as an ISO timestamp
 	deletedAt DATETIME DEFAULT NULL,
 
 	PRIMARY KEY (id),
-	FOREIGN KEY (team_id) REFERENCES team(id)
+	FOREIGN KEY (teamId) REFERENCES team(id)
 );
 
 Drop TABLE IF EXISTS open_role_members;
@@ -37,17 +37,17 @@ CREATE TABLE IF NOT EXISTS open_role_members(
 -- The UUID, stored as text
 	id TEXT NOT NULL UNIQUE COLLATE BINARY,
 	--Foreign key to the open role member (FK of open-role(id))
-	open_role_id TEXT NOT NULL,
+	openRoleId TEXT NOT NULL,
 	-- The profile id of the open role member (FK of profile(id))
-	profile_id TEXT NOT NULL,
+	profileId TEXT NOT NULL,
 	-- The open role name of the open role member (FK of open_role(open_role_name))
-	open_role_name TEXT NOT NULL,
+	openRoleName TEXT NOT NULL,
 	-- The date this open role member was added to the database, saved as an ISO timestamp
 	insertedAt DATETIME NOT NULL,
 	-- The date this open role member was deleted from the database, saved as an ISO timestamp
 	deletedAt DATETIME DEFAULT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (open_role_id) REFERENCES open_role(id),
-	FOREIGN KEY (profile_id) REFERENCES profile(id),
-	FOREIGN KEY (open_role_name) REFERENCES open_role(open_role_name)
+	FOREIGN KEY (openRoleId) REFERENCES open_role(id),
+	FOREIGN KEY (profileId) REFERENCES profile(id),
+	FOREIGN KEY (openRoleName) REFERENCES open_role(openRoleName)
 );
